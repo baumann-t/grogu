0f8695c469e900fdbc8a87d772313db86938f471
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-02-21 15:31:00 -0500

    created the standard user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Default mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
