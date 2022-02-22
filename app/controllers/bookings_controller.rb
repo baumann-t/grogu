3e4c43983629ad551c33f57759acd5fe549ecb2f
Kieran Dunch <dunch.kieran@gmail.com>
2022-02-22 13:43:12 -0500

    finished basic booking detail view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,13 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_booking, only: :show
+
+  def show; end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
 end
