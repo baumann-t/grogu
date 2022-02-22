a9ee27f7d71caa1c3accb09705c82e1caad571c8
Kieran Dunch <dunch.kieran@gmail.com>
2022-02-22 11:38:49 -0500

    finished the show user story

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,11 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: :show
+
+  def show; end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
 end
