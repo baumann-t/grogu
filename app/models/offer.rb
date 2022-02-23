4e373f88458fe33f863c3a6c76b074edc800a4c2
Huang Shewei <huangshewei@gmail.com>
2022-02-23 16:19:19 -0500

    Implemented map & markers, changed some files in order to make it work

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,8 +2,11 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :title, :price, :location, presence: true
+  # validates :title, :price, :location, presence: true
+  validates :title, :price, :address, presence: true
   validates :description, length: { minimum: 30 }
   validates :price, numericality: { greater_than: 0 }
 end
